2c7a50453149216986176c17904c3e88b1256fcb
Jason Crockett <deddokatana@gmail.com>
2014-07-10 17:53:27 +0100

    Boiler Plate Working with f11 key for Fullscreen! Woo!

--- Dev/PygameBoilerplate.py
+++ Dev/PygameBoilerplate.py
@@ -1,1 +1,47 @@
-__author__ = 'jason'
+__author__ = 'Al Sweigart,Jason Crockett'
+
+import pygame,sys
+from pygame.locals import *
+
+WIDGETORAPPNAME = "Activate Launcher"
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((640,480))
+pygame.display.set_caption(WIDGETORAPPNAME)
+
+#functions go here
+def toggle_fullscreen():
+    screen = pygame.display.get_surface()
+    tmp = screen.convert()
+    caption = pygame.display.get_caption()
+    cursor = pygame.mouse.get_cursor()  # Duoas 16-04-2007
+
+    w,h = screen.get_width(),screen.get_height()
+    flags = screen.get_flags()
+    bits = screen.get_bitsize()
+
+    pygame.display.quit()
+    pygame.display.init()
+
+    screen = pygame.display.set_mode((w,h),flags^FULLSCREEN,bits)
+    screen.blit(tmp,(0,0))
+    pygame.display.set_caption(WIDGETORAPPNAME)
+
+    pygame.key.set_mods(0) #HACK: work-a-round for a SDL bug??
+
+    pygame.mouse.set_cursor( *cursor )  # Duoas 16-04-2007
+
+    return screen #which is a pygame display
+
+
+while True: # put game logic and functions in this loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.QUIT
+            sys.exit()
+        if event.type == KEYDOWN and event.key == K_F11: # problem lines
+            toggle_fullscreen() # problem lines
+    screen = pygame.display.get_surface() # problem lines
+    pygame.display.update()
+    #pygame.display.flip() # possible extra needed.
+
